97c09469dbb380c1f3a3f34f93f6e5f6c407eef0
bendikah <36268244+bendikah@users.noreply.github.com>
2022-01-30 17:51:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,119 +1,74 @@
-import math
+import seacharts
+import AStar
+import node
+from shapely.geometry import Point
+
+import time
 
 if __name__ == '__main__':
-    import seacharts
-    from numpy import linalg as LA
-    import numpy as np
-    from shapely.geometry import Point
-    from shapely.geometry import box
-    import AStar
-    import node
-    from node import Node
+    start_time = time.time()
 
-    size = 10000, 10062                # w, h (east, north) distance in meters
-    center = 204832, 7090220          # easting/northing (UTM zone 33N)
+    size = 5000, 5000       # w, h (east, north) size of map, distance in meters
+    center = 269915, 7044135  # Frohavet archipelago north
+    resolution = 55         # number of nodes for each side of map
+
+    node_size = size[0] / resolution, size[1] / resolution  # size in meters of each grid cell
+    corner_point = center[0] - size[0] / 2, center[1] - size[1] / 2  # bottom left corner point of map
+
+    # coords used to generate front page
+    start_coords = center[0] + 1750, center[1] - 650 # start_coords = center[0] - 3500, center[1] + 3300
+    end_coords = center[0] - 2400, center[1] + 450 # end_coords = center[0] + 4300, center[1] + 40
+
+    #start_coords2 = end_coords
+    #end_coords2 = center[0] + 2000, center[1] + 4000
+
+    #start_coords3 = end_coords2
+    #end_coords3 = start_coords
+
+    # make start, end points
+    start_point = Point(start_coords[0], start_coords[1])
+    end_point = Point(end_coords[0], end_coords[1])
+
+    #start_point2 = Point(start_coords2[0], start_coords2[1])
+    #end_point2 = Point(end_coords2[0], end_coords2[1])
+
+    #start_point3 = Point(start_coords3[0], start_coords3[1])
+    #end_point3 = Point(end_coords3[0], end_coords3[1])
+
     files = ['Basisdata_50_Trondelag_25833_Dybdedata_FGDB.gdb']  # Norwegian county database name
+    enc = seacharts.ENC(size=size, center=center, files=files, new_data=False)  # enc object
 
-    enc = seacharts.ENC(size=size, center=center, files=files, new_data=False)
+    # 2D array consisting of Node objects
+    grid = node.create_grid(resolution, corner_point, node_size)
+    #grid2 = node.create_grid(resolution, corner_point, node_size)
+    #grid3 = node.create_grid(resolution, corner_point, node_size)
 
-    #bottom left corner point of map
-    corner_point = center[0]-size[0]/2, center[1]-size[1]/2
+    # draw grid on map, assign start and end nodes
+    node.draw_grid(enc, grid)
 
-    #number of nodes for each side
-    resolution = 30
-    #size in meters of each grid cell
-    node_size = size[0]/resolution, size[1]/resolution
+    start_node = node.get_start_node(grid, start_point, end_point)
+    end_node = node.get_end_node(grid, start_point, end_point)
 
-    start_coords = (204833, 7090221)
-    #enc.draw_circle(start_coords,50,'yellow', thickness=2)
-    start_point = Point(start_coords)
+    #start_node2 = node.get_start_node(grid2, start_point2, end_point2)
+    #end_node2 = node.get_end_node(grid2, start_point2, end_point2)
 
-    end_coords = (201033, 7094121)
-    #enc.draw_circle(end_coords, 50, 'green', thickness=2)
-    end_point = Point(end_coords)
+    #start_node3 = node.get_start_node(grid3, start_point3, end_point3)
+    #end_node3 = node.get_end_node(grid3, start_point3, end_point3)
 
-    #enc.draw_arrow(start_coords, end_coords, 'orange',
-    #               head_size=100, width=20, thickness=2)
+    path1 = AStar.AStarAlgorithm(start_node, end_node, grid, enc)  # gives nodes we need to visit
+    #path2 = AStar.AStarAlgorithm(start_node2, end_node2, grid2, enc)
+    #path3 = AStar.AStarAlgorithm(start_node3, end_node3, grid3, enc)
 
-    #Diagonal distance since we can move in 8 directions
-    #Note: If we limit ourselves to moving in 4 directions, use Manhattan distance.
-    #TODO: make h function
-    #dx = abs(start_coords[0]-end_coords[0])
-    #dy = abs(start_coords[1]-end_coords[1])
-
-    #movement cost c
-    c = 1
-
-    #heuristic h
-    #h = c*max(dx,dy)
-    h = c*start_point.distance(end_point)
-
-    #Euclidean distance when we can move in any direction
-    #Note: sqrt is expensive, use h**2 instead.
-    #h = math.sqrt((start_coords[0]-end_coords[0])**2
-                  #+ (start_coords[1]-end_coords[1])**2)
-    #print("h: ", h)
+    AStar.draw_line_path(start_point, end_point, path1, enc)  # draw the path
+    #AStar.draw_line_path(start_point2, end_point2, path2, enc)
+    #AStar.draw_line_path(start_point3, end_point3, path3, enc)
 
 
 
 
+    # save the image as png
+    #enc.save_image("munkholmen")
 
-    #exampleNode = Node(1,2,(-1,-1))
-    #print(exampleNode.parent)
-
-
-    #initialize grid of nodes
-    nodes = []
-
-    #creates an nxn box object b, draws it in the figure and adds it to the list rectangles
-    for i in range(resolution):
-        for j in range(resolution):
-            #don't think about why it works, it just does
-            b = box(corner_point[0] + j * node_size[0], corner_point[1] + i * node_size[1],
-                    corner_point[0] + node_size[0] + j*node_size[0], corner_point[1] + node_size[1] + i * node_size[1])
-
-            node = Node(b, 0, 0, (j, i))
-
-            #TODO: case, not if
-            #draw grid w. colors
-            """
-            if b.intersects(enc.land.geometry):
-                #enc.draw_polygon(b, 'red', fill=True)
-                x=1
-            elif b.intersects(enc.shore.geometry):
-                #enc.draw_polygon(b, 'orange', fill=True)
-                x=1
-            else:
-                x=1
-                #enc.draw_polygon(b, 'cyan', fill=False)
-
-            #draw start and end points
-            if b.intersects(start_point):
-                #enc.draw_polygon(b, 'green', fill=True)
-                x=1
-            elif b.intersects(end_point):
-                x=1
-                #enc.draw_polygon(b, 'green', fill=True)
-            """
-
-            nodes.append(node)
-
-    nodes2d = np.reshape(nodes, (resolution, resolution))
-
-    for row in nodes2d:
-        for cell in row:
-            if cell.box_object.intersects(enc.land.geometry):
-                enc.draw_polygon(cell.box_object, 'red', fill=True)
-
-            elif cell.box_object.intersects(enc.shore.geometry):
-                enc.draw_polygon(cell.box_object, 'orange', fill=True)
-            else:
-                enc.draw_polygon(cell.box_object, 'cyan', fill=False)
-
-                # draw start and end points
-            if cell.box_object.intersects(start_point):
-                enc.draw_polygon(cell.box_object, 'green', fill=True)
-            elif cell.box_object.intersects(end_point):
-                enc.draw_polygon(cell.box_object, 'green', fill=True)
+    print("My program took", time.time() - start_time, "s to run")
 
     enc.show_display()
